6b209d5eec6f4f739d36d37a04fe658786a7bfb5
= <jacobmonks0408@gmail.com>
2024-04-02 15:34:26 -0400

    added tutorial file

---
+++ Airflow/airflow_tutorial/airflow_tutorial.py
@@ -0,0 +1,69 @@
+import textwrap
+from datetime import datetime, timedelta
+from airflow.models.dag import DAG
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    "depends_on_past" : False,
+    "email" : ["airflow@example.com"],
+    "email_on_failure" : False,
+    "email_on_retry" : False,
+    "retries" : 1,
+    "retry_delay" : timedelta(minutes = 5),
+}
+
+with DAG(
+    dag_id = "my_tutorial",
+    default_args = default_args,
+    description = "A simple tutorial DAG",
+    schedule = timedelta(days = 1),
+    start_date = datetime(2023, 3, 21),
+    catchup = False,
+    tags = ["example"],
+) as dag:
+    
+    t1 = BashOperator(
+        task_id = "print_date",
+        bash_command = "date",
+    )
+
+    t2 = BashOperator(
+        task_id = "sleep",
+        depends_on_past = False,
+        bash_command = "sleep 5",
+        retries = 3,
+    )
+
+    t1.doc_md = textwrap.dedent(
+        """\
+    #### Task Documentation
+    You can document your task using the attributes 'doc_md' (markdown)
+    'doc' (plain text), 'doc_rst', 'doc_json', 'doc_yaml' which gets
+    rendered in the UI's Task Instance Details page.
+    ![img](http://montcs.bloomu.edu/~bobmon/Semesters/2012-01/491/import%20soul.png)
+    **Image Credit:** Randall Munroe [XKCD](https://xkcd.com/license.html)
+    """
+    )
+
+    dag.doc_md = """
+        This is a documentation placed anywhere
+    """
+
+    templated_command = textwrap.dedent(
+            """
+    {% for i in range(5) %}
+        echo "{{ ds }}"
+        echo "{{macros.ds_add(ds, 7)}}"
+    {% endfor %}
+    """
+    )
+
+    t3 = BashOperator(
+        task_id = "templated",
+        depends_on_past = False,
+        bash_command = templated_command,
+    )
+
+    
+
+    t1 >> [t2, t3]
