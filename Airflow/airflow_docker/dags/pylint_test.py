514616c9b3d2907235589a6e30fb5908afc9c73f
= <jacobmonks0408@gmail.com>
2025-06-05 12:11:10 -0400

    Added function for getting logaraithm.

--- Airflow/airflow_docker/dags/pylint_test.py
+++ Airflow/airflow_docker/dags/pylint_test.py
@@ -12,6 +12,10 @@ def get_sqrt(num):
     return math.sqrt(num)
 
 
+def get_log(num):
+    return math.log10(num)
+
+
 if __name__ == "__main__":
     """
     This program asks the user for an input number anf returns the square root of that number.
