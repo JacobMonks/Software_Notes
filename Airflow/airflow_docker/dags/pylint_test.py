91e0430fa0392fd14d29e85301b0c6221f8b4d50
= <jacobmonks0408@gmail.com>
2025-06-05 12:05:08 -0400

    Added comment to main function.

--- Airflow/airflow_docker/dags/pylint_test.py
+++ Airflow/airflow_docker/dags/pylint_test.py
@@ -13,5 +13,8 @@ def get_sqrt(num):
 
 
 if __name__ == "__main__":
+    """
+    This program asks the user for an input number anf returns the square root of that number.
+    """
     num = int(input("Enter your number: "))
     print("Your square root is:", get_sqrt(num))
